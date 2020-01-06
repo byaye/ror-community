030dd96577a67b1fc2d758edaf77c71266f22813
ayelenrizzalli <ayelenrizzalli@gmail.com>
2020-01-06 15:53:55 -0300

    New Controller Images - Saving description

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,17 @@
+class ImagesController < ApplicationController
+
+  def new
+  end
+
+  def create
+    #render plain: params[:image].inspect
+    @image = Image.new image_params
+    @image.save
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:description) #user can send a description
+  end
+end
