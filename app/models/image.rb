272d4427c23eaf2b22112038a0a2f3d566cf84c4
ayelenrizzalli <ayelenrizzalli@gmail.com>
2020-01-08 17:00:15 -0300

    Associating Users and Images

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,6 @@ class Image < ApplicationRecord
   mount_uploader :picture, PictureUploader
 
   validates :description, presence: true
+
+  belogs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_182839) do
+ActiveRecord::Schema.define(version: 2020_01_08_193153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2020_01_08_182839) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "picture"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_images_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +38,5 @@ ActiveRecord::Schema.define(version: 2020_01_08_182839) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "images", "users"
 end
